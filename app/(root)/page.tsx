fbd1b231877087f8fabc95ef3965a4e44f200bfb
=Hoangg Nhatt <nhathx18@gmail.com>
2024-07-27 22:23:14 +0700

    environment setup (Admin)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import { AlignCenter, AlignRight } from "lucide-react";
 import Image from "next/image";
 import { text } from "stream/consumers";
 
 export default function Home() {
   return (
-   <p>Hello Admin DashBoard</p>
+    <div className="p-4">
+      <Button size="default" variant= "outline">Click me</Button>
+    </div>
   );
 }
